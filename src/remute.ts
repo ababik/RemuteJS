dbd132f608be8c05e9d579dd40c5da7fbc73d301
Alexey Babik <alexey.babik@gmail.com>
2018-01-12 22:24:23 -0800

    Added source files

---
+++ src/remute.ts
@@ -0,0 +1,73 @@
+export { remute };
+
+const remute = <TInstance, TValue>(instance: TInstance, expression: (x: TInstance) => TValue, value: TValue) => {
+    const stack = parse(expression);
+    const reference = stack.shift();
+    if (!reference.length) return instance;
+    const mutation = compose(stack, value);
+    const result = merge(instance, mutation);
+    return result as TInstance;
+};
+
+const parse = (expression: Function) => {
+    return expression
+        .toString()
+        .replace("return", "=>")
+        .split("=>")
+        .pop()
+        .replace(/;|}/g, "")
+        .split(".")
+        .map(x => x.trim());
+};
+
+const compose = (stack: string[], value) => {
+    while (stack.length) {
+        const property = stack.pop();
+        const instance = {};
+        instance[property] = value;
+        value = instance;
+    }
+    return value;
+};
+
+const merge = (...mutations) => {
+    const instance = {};
+    while (mutations.length) {
+        const mutation = mutations.shift();
+        for (const key in mutation) {
+            if (!mutation.hasOwnProperty(key)) continue;
+            const isMutationNull = mutation[key] === null;
+            const isSame = instance[key] === mutation[key];
+
+            const isInstanceObject = isObject(instance[key]);
+            const isInstanceArray = isArray(instance[key]);
+            const isInstanceValue = !isInstanceObject && !isInstanceArray;
+            
+            const isMutationObject = isObject(mutation[key]);
+            const isMutationArray = isArray(mutation[key]);
+            const isMutationValue = !isMutationObject && !isMutationArray;
+            
+            const doAssign = () => instance[key] = mutation[key];
+            const doMerge = () => instance[key] = merge(instance[key], mutation[key]);
+            const doClone = () => instance[key] = isSame ? [...mutation[key]] : mutation[key];
+
+            if (isMutationNull) doAssign();
+
+            else if (isInstanceObject && isMutationObject) doMerge();
+            else if (isInstanceObject && isMutationArray) doAssign();
+            else if (isInstanceObject && isMutationValue) doAssign();
+
+            else if (isInstanceArray && isMutationObject) doAssign();
+            else if (isInstanceArray && isMutationArray) doClone();
+            else if (isInstanceArray && isMutationValue) doAssign();
+
+            else if (isInstanceValue && isMutationObject) doAssign();
+            else if (isInstanceValue && isMutationArray) doAssign();
+            else if (isInstanceValue && isMutationValue) doAssign();
+        }
+    }
+    return instance;
+};
+
+const isArray = (value) => Object.prototype.toString.call(value) === "[object Array]";
+const isObject = (value) => (typeof value === "object") && !isArray(value);
---
+++ test/test.ts
@@ -0,0 +1,120 @@
+import { remute } from "../src/remute.js";
+
+interface Employee {
+    readonly firstName: string;
+    readonly lastName: string;
+}
+
+interface Department {
+    readonly title: string;
+    readonly Manager: Employee;
+    readonly Employees: Employee[];
+}
+
+interface Organization {
+    readonly name: string;
+    readonly developmentDepartment: Department;
+    readonly marketingDepartment: Department;
+}
+
+const test = (test: Function) => test.call(this);
+
+test(() => {
+    const expected: Employee = {
+        firstName: "John",
+        lastName: "Doe"
+    };
+    
+    const actual = remute(expected, x => x.firstName, "Foo");
+    
+    console.assert(expected != actual);
+    console.assert(expected.firstName != actual.firstName);
+    console.assert(expected.lastName == actual.lastName);
+    console.assert("Foo" == actual.firstName);
+    console.assert("Doe" == actual.lastName);
+});
+
+test(() => {
+    const expected: Organization = {
+        name: "Organization",
+        developmentDepartment: {
+            title: "Development Department",
+            Manager: {
+                firstName: "John",
+                lastName: "Doe"
+            },
+            Employees: [
+                { firstName: "Dev", lastName: "1" },
+                { firstName: "Dev", lastName: "2" },
+            ]
+        },
+        marketingDepartment: {
+            title: "Marketing Department",
+            Manager: {
+                firstName: "Foo",
+                lastName: "Bar"
+            },
+            Employees: [
+                { firstName: "Emp", lastName: "1" },
+                { firstName: "Emp", lastName: "2" },
+            ]
+        }
+    };
+
+    const actual = remute(expected, x => x.marketingDepartment.Manager.firstName, "Brad");
+
+    console.assert(expected != actual);
+    console.assert(expected.name == actual.name);
+    console.assert(expected.developmentDepartment == actual.developmentDepartment);
+    console.assert(expected.marketingDepartment != actual.marketingDepartment);
+    console.assert(expected.marketingDepartment.title == actual.marketingDepartment.title);
+    console.assert("Marketing Department" == actual.marketingDepartment.title);
+    console.assert(expected.marketingDepartment.Manager != actual.marketingDepartment.Manager);
+    console.assert(expected.marketingDepartment.Manager.firstName != actual.marketingDepartment.Manager.firstName);
+    console.assert(expected.marketingDepartment.Manager.lastName == actual.marketingDepartment.Manager.lastName);
+    console.assert("Brad" == actual.marketingDepartment.Manager.firstName);
+    console.assert("Bar" == actual.marketingDepartment.Manager.lastName);
+});
+
+test(() => {
+    const expected: Organization = {
+        name: "Organization",
+        developmentDepartment: {
+            title: "Development Department",
+            Manager: {
+                firstName: "John",
+                lastName: "Doe"
+            },
+            Employees: [
+                { firstName: "Dev", lastName: "1" },
+                { firstName: "Dev", lastName: "2" },
+            ]
+        },
+        marketingDepartment: {
+            title: "Marketing Department",
+            Manager: {
+                firstName: "Foo",
+                lastName: "Bar"
+            },
+            Employees: [
+                { firstName: "Emp", lastName: "1" },
+                { firstName: "Emp", lastName: "2" },
+            ]
+        }
+    };
+
+    const employees = expected.developmentDepartment.Employees;
+    employees.push({ firstName: "Dev", lastName: "3" });
+
+    const actual = remute(expected, x => x.developmentDepartment.Employees, employees);
+
+    console.assert(expected != actual);
+    console.assert(expected.developmentDepartment != actual.developmentDepartment);
+    console.assert(expected.developmentDepartment.Employees != actual.developmentDepartment.Employees);
+    console.assert(3 == actual.developmentDepartment.Employees.length);
+    console.assert(expected.developmentDepartment.Employees[0] == actual.developmentDepartment.Employees[0]);
+    console.assert(expected.developmentDepartment.Employees[1] == actual.developmentDepartment.Employees[1]);
+    console.assert(expected.developmentDepartment.title == actual.developmentDepartment.title);
+    console.assert(expected.developmentDepartment.Manager == actual.developmentDepartment.Manager);
+    console.assert(expected.marketingDepartment == actual.marketingDepartment);
+});
